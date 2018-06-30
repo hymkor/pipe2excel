d579f5a308f4586283eac56df6b57f56bf9f7c76
Kaoru HAYAMA <iyahaya@nifty.com>
2018-07-01 07:50:59 +0900

    If the value matches to /^[1-9]\d*(\.\d*[1-9])?$/, set it cell as a number

--- toexcel.go
+++ toexcel.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-ole/go-ole"
 	"github.com/go-ole/go-ole/oleutil"
 	"github.com/pkg/errors"
+	"regexp"
 )
 
 type SendCsvToExcel struct {
@@ -53,6 +54,8 @@ func (this *SendCsvToExcel) NewSheet(name string) error {
 	return nil
 }
 
+var rxNumber = regexp.MustCompile(`^[1-9]\d*(\.\d*[1-9])?$`)
+
 func (this *SendCsvToExcel) Send(csv []string) error {
 	for key, val := range csv {
 		_cell, err := oleutil.GetProperty(this.sheet.IDispatch, "Cells", this.row, key+1)
@@ -60,7 +63,11 @@ func (this *SendCsvToExcel) Send(csv []string) error {
 			return errors.Wrap(err, "(*SendCsvToExcel)Send")
 		}
 		cell := _cell.ToIDispatch()
-		oleutil.PutProperty(cell, "NumberFormatLocal", "@")
+		if rxNumber.MatchString(val) {
+			oleutil.PutProperty(cell, "NumberFormatLocal", "0_")
+		} else {
+			oleutil.PutProperty(cell, "NumberFormatLocal", "@")
+		}
 		oleutil.PutProperty(cell, "Value", val)
 		cell.Release()
 	}
