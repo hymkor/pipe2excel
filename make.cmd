a7f30be0385559dd1a716c67798b0981b925f136
HAYAMA_Kaoru <iyahaya@nifty.com>
2018-07-11 18:54:10 +0900

    Impl `make upgrade`

--- make.cmd
+++ make.cmd
@@ -12,3 +12,8 @@ exit /b
     for /f %%I in ('pipe2excel -v') do set VERSION=%%I
     zip -9 pipe2excel-%VERSION%.zip pipe2excel.exe readme.md
     exit /b
+
+:"upgrade"
+    @echo off
+    for /F %%I in ('where pipe2excel') do if not "%%I" == "%~dp0pipe2excel.exe" copy /-Y /v "%~dp0pipe2excel.exe" %%I
+    exit /b
